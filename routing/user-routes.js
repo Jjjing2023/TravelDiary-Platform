39390fed9e71ec797678863feb7bc2bcea5664b5
Unknownfanzh <unknownfanzhou@gmail.com>
2023-10-14 18:56:03 -0700

    delete Tianjing's part

--- routing/user-routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getAllUsers, signup, login } from "../controllers/user-controllers.js";
-const userRouter = express.Router();
-
-userRouter.get("/", getAllUsers);
-userRouter.post("/signup", signup);
-userRouter.post("/login", login);
-export default userRouter;
