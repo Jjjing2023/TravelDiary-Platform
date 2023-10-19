e1d3e6f273f2d19df2a0cfe47e9e9645aafdbff9
Dongze Li <ezgnod@gmail.com>
2023-10-18 17:04:57 -0700

    Update db.js
    If users intend to use their own MongoDB instance for this project, it's important to set the complete MongoDB Atlas Connection String in the .env file, including all the required parameters, not just the password.

--- db/db.js
+++ db/db.js
@@ -1,6 +1,7 @@
 import { MongoClient } from 'mongodb';
 import dotenv from "dotenv";
 dotenv.config();
+// If users intend to use their own MongoDB instance for this project, it's important to set the complete MongoDB Atlas Connection String in the .env file, including all the required parameters, not just the password.
 const MONGO_URI = `mongodb+srv://admin:${process.env.MONGODB_PASSWAOORD}@cluster0.7btz9vt.mongodb.net/?retryWrites=true&w=majority`;
 export const client = new MongoClient(MONGO_URI);
 export const connectDB = async () => {
